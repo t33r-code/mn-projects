7fc6be306b78ac9fb1d399772be181cb6e1732ba
George Drayer <74135695+t33r-code@users.noreply.github.com>
2024-10-21 05:26:11 -0700

    initial commit

---
+++ src/main/java/com/t33r/udemy/Application.java
@@ -0,0 +1,10 @@
+package com.t33r.udemy;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
